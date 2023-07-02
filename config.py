ec6a417d645662fbf8fc4534f58a93f3b6b27fe0
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-03 06:24:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6050986165:AAGzJ457K6e1CPPCh5LTgenLz7gy0h8JnhY")
 API_ID = int(os.environ.get("API_ID", "22794568"))
 API_HASH = os.environ.get("API_HASH", "d3cfe756ac1275148357f3197704b280")
-SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
+SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", False))
